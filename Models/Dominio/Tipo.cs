2a2fd1ca1a7ab3fc177c6309efcfebf4abac6e17
Filipe Alves <84151293+filipe-alves-oliveira@users.noreply.github.com>
2021-09-03 19:38:47 -0300

    inserido classe Tipo

---
+++ Models/Dominio/Tipo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBCOREADS2021.Models.Dominio
+{
+    public class Tipo
+    {
+        public int id { get; set; } 
+        prop 
+    }
+}
