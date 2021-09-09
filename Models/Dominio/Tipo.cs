96e74a371d9c30b225e276ac6a386f1308215a10
Filipe Alves <84151293+filipe-alves-oliveira@users.noreply.github.com>
2021-09-09 19:55:53 -0300

    Criação da Migration Anotações_v_1

--- Models/Dominio/Tipo.cs
+++ Models/Dominio/Tipo.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WEBCOREADS2021.Models.Dominio
 {
+    [Table("Tipo")]
     public class Tipo
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("ID")]
         public int id { get; set; }
+
+        [StringLength(40)]
+        [DisplayName("Descricao")]
+        [Required(ErrorMessage ="Campo descricao é obrigatório")]
         public string descricao { get; set; }
+
         public string municipio { get; set; }
         public string outros { get; set; }
 
-    }        
+    }
 }
+
