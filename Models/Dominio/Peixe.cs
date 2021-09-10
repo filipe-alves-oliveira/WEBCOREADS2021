6e8781bcc66837f8b2e7fd7c3712bd72884f651e
Filipe Alves <84151293+filipe-alves-oliveira@users.noreply.github.com>
2021-09-10 15:53:21 -0300

    atualização

--- Models/Dominio/Peixe.cs
+++ Models/Dominio/Peixe.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,18 +7,9 @@ namespace WEBCOREADS2021.Models.Dominio
 {
     public class Peixe
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [DisplayName("ID")]
         public int id { get; set; }
-
-        [DisplayName("Nome")]
         public string nome { get; set; }
-
-        [DisplayName("Peso")]
         public float peso { get; set; }
-
-        [DisplayName("Isca")]
         public string isca { get; set; }
 
 
--- Models/Dominio/Tipo.cs
+++ Models/Dominio/Tipo.cs
@@ -1,34 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WEBCOREADS2021.Models.Dominio
 {
-    [Table("Tipo")]
-    public class Tipo
+        public class Tipo
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [DisplayName("ID")]
+
         public int id { get; set; }
-
-        [StringLength(35, ErrorMessage ="Tamanho do campo inválido", MinimumLength =5)]
-        [DisplayName("Descricao")]
-        [Required(ErrorMessage ="Campo descricao é obrigatório")]
         public string descricao { get; set; }
-
-        [StringLength(25, ErrorMessage ="Tamanho do campo municipio inválido")]
-        [Required(ErrorMessage ="campo Municipio é obrigatório")]
-        [DisplayName("Municipio")]
         public string municipio { get; set; }
-
-        [StringLength(25, ErrorMessage = "Tamanho do campo outros inválido")]
-        [Required(ErrorMessage = "campo Outros é obrigatório")]
-        [DisplayName("Outros")]
         public string outros { get; set; }
 
     }
